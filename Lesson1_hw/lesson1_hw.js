4d0d6db15ef34b4307ba01493d402fadad41d0fd
ValeriiBihun <bvp3745@gmail.com>
2021-03-29 23:13:15 +0300

    Initial commit_hw_pw_add_task

---
+++ Lesson1_hw/lesson1_hw.js
@@ -0,0 +1,298 @@
+// Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua',
+// 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+//     Вивести кожну змінну за допомогою: console.log , alert, document.write
+
+//
+// let hello = 'hello';
+// let owu = 'owu';
+// let com = 'com';
+// let ua = 'ua';
+// let one = 1;
+// let ten = 10;
+// let negNumber = -999;
+// let number1 = 123;
+// let PI = 3.14;
+// let floatNum = 2.7;
+// let number2 = 16;
+// let isTrue = true;
+// let isFalse = false;
+//
+// console.log(hello);
+// console.log(owu);
+// console.log(com);
+// console.log(ua);
+// console.log(one);
+// console.log(ten);
+// console.log(negNumber);
+// console.log(number1);
+// console.log(PI);
+// console.log(floatNum);
+// console.log(number2);
+// console.log(isTrue);
+// console.log(isFalse);
+//
+// alert(hello);
+// alert(owu)
+// alert(com);
+// alert(ua);
+// alert(one);
+// alert(ten);
+// alert(negNumber);
+// alert(number1);
+// alert(PI);
+// alert(floatNum);
+// alert(number2);
+// alert(isTrue);
+// alert(isFalse);
+//
+// document.write(hello);
+// document.write('<br/>');
+// document.write(owu);
+// document.write('<br/>');
+// document.write(com);
+// document.write('<br/>');
+// document.write(ua);
+// document.write('<br/>');
+// document.write(one);
+// document.write('<br/>');
+// document.write(ten);
+// document.write('<br/>');
+// document.write(negNumber);
+// document.write('<br/>');
+// document.write(number1);
+// document.write('<br/>');
+// document.write(PI);
+// document.write('<br/>');
+// document.write(floatNum);
+// document.write('<br/>');
+// document.write(number2);
+// document.write('<br/>');
+// document.write(isTrue);
+// document.write('<br/>');
+// document.write(isFalse);
+// document.write('<br/>');
+//
+// // - Переприсвоїти кожну змінну з завдання вище на будь які довільні значення.
+// //     Вивести кожну змінну за допомогою: console.log , alert, document.write
+//
+// hello='hello world';
+// owu='owuqwerty';
+// com='com.ua';
+// ua='uaArt';
+// one='one';
+// ten='ten';
+// negNumber=-4578;
+// number1=1111;
+// PI='Пи';
+// floatNum=27.19;
+// number2='two';
+// isTrue='true';
+// isFalse='false';
+//
+// console.log(hello);
+// console.log(owu);
+// console.log(com);
+// console.log(one);
+// console.log(ten);
+// console.log(negNumber);
+// console.log(number1);
+// console.log(PI);
+// console.log(floatNum);
+// console.log(number2);
+// console.log(isTrue);
+// console.log(isFalse);
+//
+// alert(hello);
+// alert(owu)
+// alert(com);
+// alert(ua);
+// alert(one);
+// alert(ten);
+// alert(negNumber);
+// alert(number1);
+// alert(PI);
+// alert(floatNum);
+// alert(number2);
+// alert(isTrue);
+// alert(isFalse);
+//
+// document.write(hello);
+// document.write('<br/>');
+// document.write(owu);
+// document.write('<br/>');
+// document.write(com);
+// document.write('<br/>');
+// document.write(ua);
+// document.write('<br/>');
+// document.write(one);
+// document.write('<br/>');
+// document.write(ten);
+// document.write('<br/>');
+// document.write(negNumber);
+// document.write('<br/>');
+// document.write(number1);
+// document.write('<br/>');
+// document.write(PI);
+// document.write('<br/>');
+// document.write(floatNum);
+// document.write('<br/>');
+// document.write(number2);
+// document.write('<br/>');
+// document.write(isTrue);
+// document.write('<br/>');
+// document.write(isFalse);
+// document.write('<br/>');
+
+// За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками.
+//    Вивести "Вітаю *Імя* *По батькові*. Тобі *вік* років".
+
+// const userName = prompt('enter name');
+// const userSurName = prompt('enter surname');
+// const userAge = prompt('enter age');
+// document.write('Вітаю'+' '+userName+' '+userSurName+'.');
+// document.write('<br/>');
+// document.write('Твій вік- '+userAge+"  "+'років.');
+// document.write('<br/>');
+
+// - Взяти змінні з завдання вище та зєднати їх в одну змінну (конкатинація).
+
+// let strHelloOwu=hello+owu;
+// document.write(strHelloOwu);
+// document.write('<br/>');
+// let comUa=com+ua;
+// document.write(comUa);
+// document.write('<br/>');
+// let uaOne=ua+one;
+// document.write(uaOne);
+// document.write('<br/>');
+// let helloisTrue= hello+isTrue;
+// document.write(helloisTrue);
+// document.write('<br/>');
+// let number1Hello=number1+hello;
+// document.write(number1Hello);
+
+// За допомогою prompt() отримати 3 числа. Привести їх до числа (Google "String to number JS").
+// Вивести їх в консоль.//
+
+// let firstNum=prompt('enter number1');
+// let twoNum=prompt('enter number2');
+// let threeNum=prompt('enter number3');
+// console.log('firstNum= '+firstNum.toString());
+// console.log('twoNum= '+twoNum.toString());
+// console.log('threeNum= '+threeNum.toString());
+
+//  За допомогою prompt() отримати 4 числа.
+//  Привести їх до числа за допомогою paeseInt.
+//  Додати їх всі мож собою і результат вивести в консоль.
+
+// document.write('<br/>');
+// let firstNum1=prompt('enter number1');
+// let twoNum2=prompt('enter number2');
+// let threeNum3=prompt('enter number3');
+// let fourNum4=prompt('enter number4');
+// let sumNumber=console.log('sumNumber='+(parseInt(firstNum1,10))+(parseInt(twoNum2,10))+(parseInt(threeNum3,10))+(parseInt(fourNum4,10)));
+
+// - За допомогою prompt() отримати 3 числа з плаваючою точкою за допомогою parseFloat.
+//     Просумуватиїх. Отрманий результат записати в змінну result.
+//     Вивести цю змінну в консоль.
+
+// let floatNum1=prompt('enter float number1');
+// let floatNum2=prompt('enter float Number2');
+// let floatNum3=prompt('enter float Number3');
+// let result=parseFloat(floatNum1)+parseFloat(floatNum2)+parseFloat(floatNum3);
+// console.log(result);
+
+// - За допомогою prompt() отримати 3 числа з плаваючою точкою.
+// Заокруглити їх (Google "round number JS").
+// Просумувати їх між собою. Результат вивести в консоль.
+
+// let floatNumb1 = prompt('enter float number1');
+// console.log(floatNumb1);
+// let floatNumb2 = prompt('enter float Number2');
+// console.log(floatNumb2);
+// let floatNumb3 = prompt('enter float Number3');
+// console.log(floatNumb3);
+// let sumAllRoudNum = Math.round(floatNumb1) + Math.round(floatNumb2) + Math.round(floatNumb3);
+// console.log('sumAllRoudNum= ' + sumAllRoudNum);
+
+// - За допомогою prompt() отримати 2 цілих числа.
+//     Перше число - це число яке будуть підносити до степеня.
+//     Друге число - це число яке являється степенню.
+
+// let numb1 = prompt('enter  number1(base)');
+// console.log(numb1);
+// let intNumb1=parseInt(numb1,10);
+// console.log('Основа степені  : '+intNumb1);
+// let numb2 = prompt('enter number2(exponent');
+// console.log(numb2);
+// let intNumb2=parseInt(numb2,10);
+// console.log('Показник степені : '+intNumb2);
+
+// За допомогою Math.pow (Google) піднести перше число в степінь другого числа.
+//     Example. Число 1 = 5. Число 2 = 2. Результат = 25
+
+// let baseNumb = 5;
+// console.log('основа степені: ' + baseNumb);
+// let exponentNumb = 2;
+// console.log('показник степені:' + exponentNumb);
+// let powNumb = Math.pow(baseNumb, exponentNumb);
+// console.log('результат піднесення в степінь: ' + powNumb);
+
+// За допомогою оператора typeof визначити типи наступних змінних та вивестиїх в консоль.
+//     let a = 100; let b = '100'; let c = true;
+
+// let a = 100;
+// let b = "100";
+// let c = true;
+// console.log('a: '+typeof(a));
+// console.log('b: '+typeof (b));
+// console.log('c: '+typeof (c));
+
+// - Поставте відповідний оператор в виразах щоб вийшов відповідний результат.
+//     В однакових виразах не використовувати однакові оператори!!!
+
+//     5 ? 6 ->true
+// 5 ? 6 ->false
+// 5 ? 6 ->false
+// 5 ? 6 ->false
+// 10 ? 10 ->true
+// 10 ? 10 ->true
+// 10 ? 10 ->false
+// 10 ? 10 ->false
+// 10 ? 10 ->false
+// 123 ? '123' ->false
+// 123 ? '123' ->true
+
+// console.log(5>6);
+// console.log(5==6);
+// console.log(5>=6);
+// console.log(10==10);
+// console.log(10===10);
+// console.log(10<10);
+// console.log(10>10);
+// console.log(10!=10);
+// console.log(123==='123');
+// console.log(123=='123');
+
+// Додатково:
+//     - Подивіться на наступнівирази і спробуйте вгадатияким буде вивід в консоль.
+//     Перевірте себе.
+
+// console.log(132 > 100 && 45 < 12 );-- false
+// console.log(34 > 33 && 23 < 90 );-true
+// console.log(99 > 100 && 45 > 12 ); ---false
+// console.log(132 > 100 || 45 < 12 ); --true
+// console.log(111 > 11 || 45 < 111 ); --true
+// console.log((111 > 11 || 45 < 111) && (132 > 100 || 45 < 12) );--true
+// console.log((111 > 11 || 45 < 56) || (132 > 100 || 45 < 12) ); - true
+// console.log((132 > 100 && 45 < 12 )&& (132 > 100 || 45 < 12 ) );--false
+// console.log((111 > 11 || 45 < 111) || (99 > 100 && 45 > 12 ));--true
+// console.log(!!'-1');--true
+// console.log(!!-1);-- true
+// console.log(!!'0');true
+// console.log(!!'null');--true
+// console.log(!!'undefined'); --true
+// console.log(!!(3/'owu')); -- false
+// console.log((111 > 11 || 45 < 111) ||  !!'0');--true
+// console.log((!!111 == !!11 || 45 < 111) && (99 > 100 && 45 > 12 ));--false
+
